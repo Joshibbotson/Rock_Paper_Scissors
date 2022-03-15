f5f6b62b1d7af314742ef51e81ece9bdc6aa4d6b
Joshibbotson <joshua_ibbotson@hotmail.com>
2022-03-15 19:31:12 +0000

    redoing game from scratch in test-window

--- app.js
+++ app.js
@@ -1,33 +1,12 @@
-let userChoice
-let display = console.log(userChoice)
-const possibleChoices = document.querySelectorAll('button')
+const inventory = [
+'Long Sword', 
+'Shield',
+'Double headed axe',
+'Chainmail',
+'Mythic arrows'];
 
-
-possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
-    userChoice = e.target.id
-    display = userChoice
-}))
-
-
-
-
-/*
-generateComputerChoice()
-let userChoice = (prompt());
-let computerChoice 
-let result = null;
-
-function generateComputerChoice () {
-const randomNumber = Math.floor(Math.random() * 3) + 1
-
-    if (randomNumber === 1) {
-        computerChoice = 'rock'
-    }
-    if (randomNumber === 2) {
-        computerChoice ='paper'
-    }
-    if (randomNumber === 3) {
-        computerChoice = 'scissors'
-    }
+function chosenItem () {
+    const itemChoice = (Math.random() * inventory.length)
 }
-console.log (computerChoice);*/
+
+chosenItem();
