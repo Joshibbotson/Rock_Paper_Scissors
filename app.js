61918ebebcf2bb4c7b0750de349591cb4c565588
Joshibbotson <joshua_ibbotson@hotmail.com>
2022-03-27 19:03:52 +0100

    final update, project finished, best of 5 implemented, score tracker implemented, final winner implemented.

--- app.js
+++ app.js
@@ -1,12 +1,56 @@
-const inventory = [
-'Long Sword', 
-'Shield',
-'Double headed axe',
-'Chainmail',
-'Mythic arrows'];
+//
+// const itemsArray = ["rock", "paper", "scissors"];
 
-function chosenItem () {
-    const itemChoice = (Math.random() * inventory.length)
+// const playerSelection = prompt("Rock, paper or scissors?").toLowerCase;
+// const computerSelection = computerPlay();
+
+// function computerPlay() {
+//   return [Math.floor(Math.random() * itemsArray.length)];
+// }
+
+// function playRound(playerSelection, computerSelection) {
+//   if (playerSelection == computerSelection) {
+//     console.log("draw");
+//   } else if (
+//     (playerSelection == "rock" && computerSelection == "paper") ||
+//     (playerSelection == "paper" && computerSelection == "scissors") ||
+//     (playerSelection == "scissors" && computerSelection == "rock")
+//   ) {
+//     console.log("You lose");
+//   } else {
+//     console.log("you win");
+//   }
+// }
+// playRound(playerSelection, computerSelection);
+
+// for (let i = 0; i < itemsArray.length; i++) {
+//   console.log(computerPlay());
+// }
+let score = 2;
+for (let i = 0; i < 3; i++) {
+  game();
+  console.log(score);
 }
 
-chosenItem();
+function game() {
+  const answer = confirm("true or false?");
+  let item = answer;
+  let withinBox;
+  let test = withinBox;
+
+  switch (test) {
+    case true:
+      return score++;
+    default:
+      return score--;
+  }
+  makeItemTrue(item);
+
+  function makeItemTrue(decision) {
+    if ((decision = true)) {
+      return (withinBox = true);
+    } else if ((decision = false)) {
+      return (withinBox = false);
+    }
+  }
+}
